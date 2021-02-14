b2da4b1d05d76c060dd1fffaf6cb43c00a6ba2c1
diparyal <diparyal@protonmail.com>
2021-02-14 21:21:54 +0545

    ajax approved Order

--- dashboard/views.py
+++ dashboard/views.py
@@ -16,30 +16,53 @@ def home(request):
     buyers = Order.objects.filter(supplier__isnull=True,status='pending')
     # print(buyers)
     suppliers = Order.objects.filter(buyer__isnull=True,status='pending')
+    app_order = Order.objects.filter(status='approved')
+
     context={'buyers':buyers, 'suppliers':suppliers ,'card':{'Product':pro_count,
-    'Order':order_count,'Buyer Order': buyers.count(),'Suppliers Order':suppliers.count() }}
+    'Order':order_count,'Buyer Order': buyers.count(),'Suppliers Order':suppliers.count() }
+    ,'app_order': app_order}
     return render(request,'dashboard/home.html',context)
 
 
 def statusupdate(request):
     # staff = StaffRequest.objects.get(id=pk).update(status='Approved') OR
     data = json.loads(request.body)
-    print(data)
+    # print(data)
     productId = data['productId']
     action = data['action']
-    staff = Order.objects.get(id=productId)
+    approve_order = Order.objects.get(id=productId)
 
     if action == 'Approve':
-        staff.status = 'approved'
+        approve_order.status = 'approved'
     elif action == 'Decline':
-        staff.status = 'decline'
+        approve_order.status = 'decline'
     else:
         pass
 
-    staff.save()
+    username=''
+
+    if approve_order.supplier != None  :
+        username= approve_order.supplier.username
+    else:
+        username= approve_order.buyer.username
+
+    app_detail={
+        'id':approve_order.id,
+        'username': username,
+        'product':approve_order.product.name,
+        'units': approve_order.units,
+    }
+
+
+    # if approve_order.is_seller:
+    #     username= 
+
+    # approve_order={'id':staff.id,'username':staff.username}
+
+    approve_order.save()
 
     # return JsonResponse('Payment submitted..', safe=False)
-    return JsonResponse({ 'productId':productId})
+    return JsonResponse({ 'productId':productId,'app_detail':app_detail })
 
 
     # return redirect('home')
